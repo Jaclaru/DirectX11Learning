32c9923c8d1c153b102a7418c3a0eb434b46e2d7
Jaclaru <h879822739@sina.com>
2022-10-08 22:17:54 +0800

    添加light相关代码

---
+++ chapter07/main.cpp
@@ -0,0 +1,27 @@
+
+#include "D3DApp.h"
+#include "D3DUtil.h"
+#include "D3DApp.h"
+#include "DXTrace.h"
+
+using namespace DirectX;
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance,
+    _In_ LPSTR cmdLine, _In_ int showCmd)
+{
+    // 这些参数不使用
+    UNREFERENCED_PARAMETER(prevInstance);
+    UNREFERENCED_PARAMETER(cmdLine);
+    UNREFERENCED_PARAMETER(showCmd);
+    // 允许在Debug版本进行运行时内存分配和泄漏检测
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    /*ShapesApp theApp(hInstance, L"Rendering a Box", 1280, 720);
+
+    if (!theApp.Init())
+        return 0;
+
+    return theApp.Run();*/
+}
